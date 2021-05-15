1b5ed133e4533bc0cdc8a55b9dbbe9738adbda7e
chedi hammami <chedihammami9@gmail.com>
2021-05-15 11:49:40 +0100

    adding stores

--- src/app/components/stores-list/stores-list.component.ts
+++ src/app/components/stores-list/stores-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from 'src/app/Models/Store.model';
+import { StoresService } from 'src/app/services/stores.service';
 
 @Component({
   selector: 'app-stores-list',
@@ -6,11 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stores-list.component.scss']
 })
 export class StoresListComponent implements OnInit {
-
-  constructor() { }
+  pageNumber:number=1
+  stores:Array<Store>
+  constructor(
+    private storesservice:StoresService,
+    private activatedRoute:ActivatedRoute,
+    private router:Router) { }
 
   ngOnInit(): void {
-    
+    this.activatedRoute.params.subscribe((params)=>
+    {
+      if (params['pageNumber'])
+      {this.pageNumber=params['pageNumber']}
+      
+      this.storesservice.getStores(this.pageNumber).subscribe(
+        (response)=>{this.stores=response;console.log(this.stores)},
+        (error)=>{console.log(error)},
+      )
+    })
+  }
+  nextPage()
+  {
+    this.pageNumber++;
+    const link=['stores',this.pageNumber]
+    this.router.navigate(link)
+  }
+  previousPage()
+  {
+    if (this.pageNumber>1)
+    {
+      this.pageNumber--;
+    }
+    const link=['stores',this.pageNumber]
+    this.router.navigate(link)
   }
 
 }
